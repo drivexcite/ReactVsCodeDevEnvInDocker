44379332b25111ed94fc04b46af969cd54e9891a
Tona Diaz <drivexcite@gmail.com>
2019-12-16 16:30:30 -0700

    Refactored by eslint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
@@ -29,6 +29,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
